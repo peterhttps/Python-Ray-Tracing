b7faf96650472d5c3c7243f85d28de0bc52985b3
Pedro Lucas <plrf100@gmail.com>
2021-09-19 22:00:46 -0300

    feature(antialiasing): implementing antialiasing

--- src/main.py
+++ src/main.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 from vector import *
 from geometries import *
 import math
-
+import random
+from progress.bar import Bar
 
 # Image
 aspectRatio = 16 / 9
 width = 800
 height = math.trunc(width / aspectRatio)
-image = np.zeros((height, width, 3))
 
 # Camera
 viewportHeight = 2
@@ -22,42 +22,65 @@ lowerLeftCorner = origin - (horizontal / 2) - (vertical / 2) - vec3(0.0, 0.0, fo
 
 print("Imagem size %d x %d" % (width, height))
 
-def backgroundColor(dir):
+def marscolor(dir):
   t = 0.5 * (dir[1] + 1.0)
   return (1.0 - t) *  np.array([0.7, 0.8, 0.9]) + t*np.array([0.05, 0.05, 0.2])
+
+def backgroundColor(dir):
+  t = 0.5 * (dir[1] + 1.0)
+  return (1.0 - t) *  np.array([1.0, 1.0, 1.0]) + t*np.array([0.5, 0.7, 1.0])
+
 
 def raycolor(ray, scenelist):
   record = HitRecord()
 
   # hitSphere(sphere, ray, 0.0, np.Infinity, record)
 
-  orgn = np.array(ray.origin)
-  dir = np.array(ray.direction)
-  while (sceneListhit(scenelist, Ray(orgn, dir), 0.0001, np.Infinity, record)):
-    dir = unitvector(reflect(dir, record.normal))
-    orgn = record.p
+  if (sceneListhit(scenelist, ray, 0.0001, np.Infinity, record)):
+    color = 0.5 * (record.normal + 1.0)
+    return (color)
 
-  return backgroundColor(dir)
+  # orgn = np.array(ray.origin)
+  # dir = np.array(ray.direction)
+  # while (sceneListhit(scenelist, Ray(orgn, dir), 0.0001, np.Infinity, record)):
+  #   dir = unitvector(reflect(dir, record.normal))
+  #   orgn = record.p
+
+  return backgroundColor(ray.direction)
+  # return marscolor(dir)
 
 s1 = Sphere(vec3(0.0, 0.0, -1.0), 0.5)
-s2 = Sphere(vec3(-1.0, 0.0, -2.0), 0.5)
-s3 = Sphere(vec3(0.5, 1.5, -1.5), 0.5)
-bigradius = 1000
+# s2 = Sphere(vec3(-1.0, 0.0, -2.0), 0.5)
+# s3 = Sphere(vec3(0.5, 1.5, -1.5), 0.5)
+bigradius = 100.0
 floor = Sphere(vec3(0.0, -bigradius - 0.5, -1), bigradius)
 
 world = SceneList()
 world.push(s1)
-world.push(s2)
-world.push(s3)
+# world.push(s2)
+# world.push(s3)
 world.push(floor)
 
-for j in range(height):
-  for i in range(width):      
-    u = (i - 1) / (width - 1)
-    v = 1.0 - (j - 1) / (height - 1)
-    direc = lowerLeftCorner + u*horizontal + v*vertical - origin 
-    ray = Ray(origin, direc)
+def render(samplesPerPixel=100):
+  image = np.zeros((height, width, 3))
+  bar = Bar('Rendering', max=height, suffix='%(percent)d%%')
+  
+  for j in range(height):
+    for i in range(width):
+      pixelColor = vec3(0.0, 0.0, 0.0)
+      for n in range(samplesPerPixel):
 
-    image[j, i] = np.clip(raycolor(ray, world), 0, 1)
-  print(j, height)
-plt.imsave('src/rendered/image8.png', image)
+        u = (i - 1 + random.random()) / (width - 1)
+        v = 1.0 - (j - 1 + random.random()) / (height - 1)
+        direc = lowerLeftCorner + u*horizontal + v*vertical - origin 
+        ray = Ray(origin, direc)
+
+        pixelColor += raycolor(ray, world) 
+
+      image[j, i] = np.clip(pixelColor /samplesPerPixel, 0, 1)
+    bar.next()    
+
+  return image
+
+frame = render()
+plt.imsave('src/rendered/AAimage9.png', frame)
